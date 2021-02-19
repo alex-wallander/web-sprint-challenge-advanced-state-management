d86bdadf9b84a96275ee79d35cd37a1ed7bf7f4f
alex-wallander <alexwallander@gmail.com>
2021-02-19 11:15:23 -0600

    fetchSmurfs is connected to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
+import { fetchSmurfs } from './actions/index';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
@@ -22,7 +24,15 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    isLoading: state.isLoading,
+    error: state.error
+  };
+}
+
+export default connect(mapStateToProps, {fetchSmurfs})(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,35 @@ export const SMURF_FETCH_FAIL = 'SMURF_FETCH_FAIL';
 export const ADD_SMURF = 'ADD_SMURF';
 export const ERROR_VALUE = 'ERROR_VALUE';
 
+export const fetchSmurfs = () => {
+    return dispatch => {
+        dispatch({ type:SMURF_FETCH});
+
+        axios
+        .get('http://localhost:3000/')
+        .then(res => {
+            dispatch({type: SMURF_FETCH_SUCCESS, payload: res.data.smurfs})
+        })
+        .catch(err => {
+            dispatch({type: SMURF_FETCH_FAIL, payload: err.Response.code})
+        })
+    }
+}
+
+export const addSmurf = (smurf) => {
+    return({
+        type: ADD_SMURF,
+        payload: smurf
+    })
+}
+
+export const errorValue = (error) => {
+    return({
+        type: ERROR_VALUE,
+        payload: error
+    })
+}
+
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
